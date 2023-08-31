2577c83ae5292e6b5e6dd3e6e8ed2fe6fcf3bb1f
kehav1534 <90709089+kehav1534@users.noreply.github.com>
2023-08-31 18:32:07 +0530

    Update mytimer.py

--- SourceCode/pythonCode/mytimer.py
+++ SourceCode/pythonCode/mytimer.py
@@ -1,3 +1,5 @@
+#Source code by:- Keshav Maheshwari <keshav1534@gmail.com>
+import sys
 from pynput import keyboard 
 import time
 import threading
@@ -5,10 +7,11 @@ import os
 import win32api
 import psutil
 from plyer import notification
+import json
 
 recent = time.time()
 older_time = time.time()
-myapp = "Reminder"
+myapp = "ChronoAlert"
 file_path = 'C:\Program Files\ChronoAlert\icon'    #File path location of Shut_Down Timer Folder.
 
 def keys():
@@ -34,9 +37,62 @@ def cursor():
             savedpos = curpos  
         time.sleep(0.5)
 
+
+#Reads the notification.json get the notification messages.
+def readJson(path="notification.json", data=None):
+    
+    with open(path, "r+") as file:
+        notifi_data = json.load(file)
+        return notifi_data
+
+notification_list = readJson()
+
+def notify():
+    #These are some of my reminders. You can also add your custom reminders.
+    # You can make your custom reminder by adding a it into above list
+    #                               
+    def notifications(n):
+        while True:
+            time.sleep(notification_list[n]["sleep"])
+            notification.notify(
+                title  = notification_list[n]["title"],
+                message = notification_list[n]["message"],
+                app_icon =  file_path+"\\"+notification_list[n]["app_icon"],
+                timeout = notification_list[n]["timeout"],
+                app_name = myapp
+            )
+            
+    #schedule        
+    def rest_notification():
+        while True:
+            #Send Notification after 5 hours and put system to hybernation in next 15min.
+            #Save your work before time runs out.
+            time.sleep(18000)
+            notification.notify(
+                title = "Working for too long...@-@",
+                message = "Take Some rest. Going to Hybernation in 15 minutes.",
+                app_icon = file_path+"\\systemlockscreen_104197.ico" ,
+                timeout = 10,
+                app_name = myapp
+            )
+            time.sleep(900)
+            os.system(r'rundll32.exe powrprof.dll,SetSuspendState Hibernate')
+
+
+
+    #Made threads of notifications so that they keep running in parallel-like fasion.
+    for mesg in range(len(notification_list)):
+        notify1 = threading.Thread(target= notifications, args=(mesg, ))
+        notify1.start()
+    notify3 = threading.Thread(target=rest_notification)
+    #Start your threads to make them run, or else won't work.
+    
+    notify3.start()
+
+
 def shutdown():
     #Shut-down windows operating system.
-    os.system('shutdown -s -t 0')  
+    os.system('shutdown -s -t 0')
 
 
 def check_user():                   
@@ -69,58 +125,6 @@ def check_user():
             shutdown()
         time.sleep(1)
 
-
-def notify():
-    #These are some of my reminders. You can also add your custom reminders.
-    # You can make your custom reminder by adding a notification function
-    # And adding Thread I did in line near 122(Look by yourself.)
-    # Do not forget to intialize the thread or else your function won't work.                               
-    def water_notification():
-        while True:
-            time.sleep(3600)
-            notification.notify(
-                title  = "Time to Drink!!",
-                message = "Drink some water to stay hydrated!",
-                app_icon =  file_path+"\water_drink_bottle_icon.ico",
-                timeout = 10,
-                app_name = myapp
-            )
-            
-    def exercise_notification():
-        while True:
-            time.sleep(5400)
-            notification.notify(
-                title = "Move yourself!!",
-                message = "Exercise a little or go out for a nice walk. ^_^",
-                app_icon =  file_path+"\walk_icon_151006.ico",
-                timeout =10,
-                app_name = myapp
-            )
-            
-    def rest_notification():
-        while True:
-            #Send Notification after 5 hours and put system to hybernation in next 15min.
-            #Save your work before time runs out.
-            time.sleep(18000)
-            notification.notify(
-                title = "Working for too long...@-@",
-                message = "Take Some rest. Going to Hybernation in 15 minutes.",
-                app_icon = file_path+"\\systemlockscreen_104197.ico" ,
-                timeout = 10,
-                app_name = myapp
-            )
-            time.sleep(900)
-            os.system(r'rundll32.exe powrprof.dll,SetSuspendState Hibernate')
-
-    #Made threads of notifications so that they keep running in parallel-like fasion.
-    notify1 = threading.Thread(target= water_notification)
-    notify2 = threading.Thread(target=exercise_notification)
-    notify3 = threading.Thread(target=rest_notification)
-    #Start your threads to make them run, or else won't work.
-    notify1.start()
-    notify2.start()
-    notify3.start()
-
 def sysBattery():           
     #ShutDown the system when battery is lower than 7% and 
     #if charger is not plugged-in.
@@ -130,7 +134,7 @@ def sysBattery():
         if battery.percent<=7 and battery.power_plugged == False:
             notification.notify(
                 title = "Charging below 7%",
-                message = "Plug-in the charger. Switching-Off in 2 minutes.",
+                message = "Plug-in the charger. Switching-Off at 5%",
                 app_icon = file_path+"\com_94378.ico",
                 timeout = 15,
                 app_name = myapp
@@ -140,21 +144,22 @@ def sysBattery():
                 shutdown()
         time.sleep(10)
 
+
 if __name__ == "__main__":
-    notification.notify(
-        title = "Reminder",
-        message = "Application running in background.",
-        app_icon = file_path+"\emoji_laughing_fill_icon_185681.ico" ,
-        timeout = 10,
-        app_name = myapp
-    )
-    t1 = threading.Thread(target=keys)
-    t2 = threading.Thread(target=notify)
-    t3 = threading.Thread(target =check_user)
-    t4 = threading.Thread(target=sysBattery)
-    t5 = threading.Thread(target=cursor)
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
+    if len(sys.argv) > 1:
+        notification.notify(
+            title = myapp,
+            message = "Application running in background.",
+            app_icon = file_path+"\emoji_laughing_fill_icon_185681.ico" ,
+            timeout = 10,
+            app_name = myapp
+        )
+
+        #func_list contains the list of functions which are required to be threaded
+        func_list = (keys, notify, check_user, sysBattery, cursor)
+        for func in func_list:
+                thread = threading.Thread(target=func)
+                thread.start()
+
+    else:
+        os.system("C:/Users/Keshav^ Maheshwari/Desktop/ChronoAlert_/Reminder_vbs.vbs") ##Need to chage this <-------------change
